51c505f8fcf0c4a81026fe4772d407729a166846
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-08 13:26:41 -0700

    add test for recursive implementation of is_in function

--- MIT/6.00.1x/week2/test_week2.py
+++ MIT/6.00.1x/week2/test_week2.py
@@ -16,6 +16,16 @@ TEST_GCD = [
 (198, 396, 198),
 ]
 
+TEST_IS_IN = [
+('a', '', False),
+('v', 'chhjkpvwwxz', True),
+('j', 'bj', True),
+('o', 'behlottvw', True),
+('r', 'bdgiilpvwx', False),
+('i', 'aahiiikmmnnnoqwxz', True),
+('k', 'aabcdggppx', False),
+]
+
 
 @pytest.mark.parametrize('a, b, result', TEST_GCD)
 def test_iter_gcd(a, b, result):
@@ -29,3 +39,9 @@ def test_recur_gcd(a, b, result):
     """Test recur_gcd returns correct gcd by using recursion."""
     from gcd_iter_vs_recur import recur_gcd
     assert recur_gcd(a, b) == result
+
+@pytest.mark.parametrize('a, b, result', TEST_IS_IN)
+def test_is_in(a, b, result):
+    """Test is_in returns correct result."""
+    from is_in import is_in
+    assert is_in(a, b) == result
