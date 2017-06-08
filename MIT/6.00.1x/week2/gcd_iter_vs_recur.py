83275eaf5dd5b1d8fc389f1e7b296356708932bf
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-08 10:00:15 -0700

    add solution for finding gcd showing iterative vs recursive approach

---
+++ MIT/6.00.1x/week2/gcd_iter_vs_recur.py
@@ -0,0 +1,15 @@
+def iter_gcd(a, b):
+    '''
+    a, b: positive integers
+
+    returns: a positive integer, the greatest common divisor of a & b.
+    '''
+    def isprime(n):
+        if n % 2 == 0 or n == 1:
+            return False
+        return all(n % i for i in range(3, int(n**0.5) + 1, 2))
+    if isprime(a) or isprime(b): return 1
+    smaller = a if a < b else b
+    for div in range(smaller, 1, -1):
+        if a % div == 0 and b % div == 0:
+            return div
