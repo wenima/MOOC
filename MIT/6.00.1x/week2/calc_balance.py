3d18bc9904cf7581aec4b08d1585c2d985279b52
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-09 15:21:53 -0700

    scaffold commit

---
+++ MIT/6.00.1x/week2/calc_balance.py
@@ -0,0 +1,10 @@
+"""Solution for Problem 1 of Week 2 Problem Set - Paying off Debt on CC
+Write a program to calculate the credit card balance after one year if a person
+only pays the minimum monthly payment required by the credit card company each month.
+"""
+
+def calc_balance(balance, annual_interest, minimum_payment):
+    """Return the balance at the end 12 months given starting balance, annual interesting
+    and minimum_payment.
+    """
+    pass
