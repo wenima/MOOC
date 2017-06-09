bed53dd0a086d5bc0fdf6640813efc36bedd9298
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-09 16:08:26 -0700

    add function to calc cc balance with just min payments

--- MIT/6.00.1x/week2/calc_balance.py
+++ MIT/6.00.1x/week2/calc_balance.py
@@ -3,8 +3,11 @@ Write a program to calculate the credit card balance after one year if a person
 only pays the minimum monthly payment required by the credit card company each month.
 """
 
-def calc_balance(balance, annual_interest, minimum_payment):
+def calc_balance(balance, annual_interest, minimum_payment, months=12):
     """Return the balance at the end 12 months given starting balance, annual interesting
     and minimum_payment.
     """
-    pass
+    for _ in range(0, months):
+        balance = balance - balance * minimum_payment
+        balance = balance + balance * annual_interest/12
+    return round(balance, 2)
