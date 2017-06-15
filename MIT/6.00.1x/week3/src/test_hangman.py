afbe2fef80b7ddd65de778da15d3997901875b1e
MarcMBPDev <marc.wenicker@gmail.com>
2017-06-15 15:36:58 -0700

    add test for guessed words and avail letters

--- MIT/6.00.1x/week3/src/test_hangman.py
+++ MIT/6.00.1x/week3/src/test_hangman.py
@@ -8,8 +8,30 @@ WORD_GUESS = [
 ('apple', ['e', 'i', 'x', 'p', 'l', 'a'], True),
 ]
 
+LETTERS_GUESSED = [
+('apple', ['e', 'i', 'k', 'p', 'r', 's'], '_ pp_ e'),
+('apple', ['m', 'n', 'x', 'o'], '_ _ _ _ _ '),
+]
+
+AVAIL_LETTERS = [
+(['e', 'i', 'k', 'p', 'r', 's'], 'abcdfghjlmnoqtuvwxyz'),
+]
+
 @pytest.mark.parametrize('s, l, result', WORD_GUESS)
 def test_isWordGuessed(s, l, result):
     """Test isWordGuessed return correct result."""
     from hangman import isWordGuessed
     assert isWordGuessed(s, l) == result
+
+
+@pytest.mark.parametrize('s, l, result', LETTERS_GUESSED)
+def test_getGuessedWord(s, l, result):
+    """Test getGuessedWord return correct result."""
+    from hangman import getGuessedWord
+    assert getGuessedWord(s, l) == result
+
+@pytest.mark.parametrize('s, result', AVAIL_LETTERS)
+def test_getAvailableLetters(s, result):
+    """Test getGuessedWord return correct result."""
+    from hangman import getAvailableLetters
+    assert getAvailableLetters(s) == result
